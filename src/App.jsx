f144dbc97b1232ffaa8156eefc310785b39fdc06
Devadathan M B <devadathanmb@gmail.com>
2023-05-22 14:45:56 +0530

    wrap navbar inside authprovider

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,27 @@ import Signup from "./components/SignUp/Signup";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Settings from "./components/SettingsPage/Settings";
 import Diet from "./components/DietPage/Diet";
+import { AuthProvider } from "./context/AuthContext";
+import NavbarSample from "./components/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Signup} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/settings" component={Settings} />
-          <Route exact path="/diet" component={Diet} />
-        </Switch>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <NavbarSample />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Signup} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/settings" component={Settings} />
+            <Route exact path="/diet" component={Diet} />
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
