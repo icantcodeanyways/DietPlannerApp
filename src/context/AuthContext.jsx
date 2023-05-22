28272e8cf205f488a21b57edd94b9adf39975353
Devadathan M B <devadathanmb@gmail.com>
2023-05-22 15:01:09 +0530

    format prettier

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -6,7 +6,7 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
-    <AuthContext.Provider value={[ isAuthenticated, setIsAuthenticated ]}>
+    <AuthContext.Provider value={[isAuthenticated, setIsAuthenticated]}>
       {children}
     </AuthContext.Provider>
   );
