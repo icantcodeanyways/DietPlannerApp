10f97432861800d0683d317cd9802b44f55082c8
Devadathan M B <devadathanmb@gmail.com>
2023-05-24 20:44:54 +0530

    implement manual food logging (bugs present)

--- src/components/DietPage/DietBox.jsx
+++ src/components/DietPage/DietBox.jsx
@@ -6,6 +6,9 @@ import {
   Container,
   Form,
   CardGroup,
+  InputGroup,
+  Row,
+  Col,
   ToggleButtonGroup,
   ToggleButton,
 } from "react-bootstrap";
@@ -19,6 +22,10 @@ import { toast } from "react-toastify";
 function DietBox() {
   const [showRecommend, setShowRecommend] = useState(false);
   const [showLogFood, setShowLogFood] = useState(false);
+
+  const [completionLoading, setCompletionLoading] = useState(false);
+
+  const [foodItems, setFoodItems] = useState([{ food: "", quantity: "" }]);
 
   const [loading, setLoading] = useState({ status: false, message: "" });
   const [isDietGenerated, setIsDietGenerated] = useState(false);
@@ -50,8 +57,93 @@ function DietBox() {
     preFetch();
   }, []);
 
+  const handleQuantityChange = (e, index) => {
+    const value = e.target.value;
+    setFoodItems((prevFoodItems) => {
+      const updatedFoodItems = [...prevFoodItems];
+      updatedFoodItems[index].quantity = value;
+      return updatedFoodItems;
+    });
+  };
+
+  const handleAddRow = () => {
+    setFoodItems((prevFoodItems) => [
+      ...prevFoodItems,
+      { food: "", quantity: "" },
+    ]);
+  };
+
+  const handleRemoveRow = () => {
+    if (foodItems.length === 1) {
+      setFoodItems([{ food: "", quantity: "" }]);
+      console.log(foodItems);
+      setSuggestions([]);
+      return;
+    }
+    setFoodItems((prevFoodItems) => {
+      const updatedFoodItems = [...prevFoodItems];
+      updatedFoodItems.pop();
+      return updatedFoodItems;
+    });
+  };
+
+  const handleFoodLog = async () => {
+    const requestBodyFoodItems = foodItems.map((item) => {
+      if (!item.quantity || item.quantity.toString() == "0") {
+        toast.error("Please provide a valid quantity");
+        return;
+      }
+      return {
+        food: item.food[0],
+        quantity: item.quantity,
+      };
+    });
+
+    const requestBody = {
+      meal_timing: mealTiming,
+      log_info: requestBodyFoodItems,
+    };
+
+    try {
+      const token = localStorage.getItem("Token");
+      const userId = jwt_decode(token).user_id;
+      setLoading({ status: true, message: "Saving diet plan.." });
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_ENDPOINT}/api/users/${userId}/custom_food_log`,
+        requestBody,
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+      setLoading({ status: false, message: "" });
+      setFoodItems([{ food: "", quantity: "" }]);
+      console.log(response);
+      toast.success("Diet logged successfully");
+    } catch (error) {
+      setLoading({ status: false, message: "" });
+      setFoodItems([{ food: "", quantity: "" }]);
+      if (error.response.status === 400) {
+        toast.error("Unable to log diet info due to missing data.");
+      } else if (error.response.status === 409) {
+        toast.error(
+          "Another diet plan already exist for the selected meal time. Please select a different time and try"
+        );
+      } else {
+        console.log(error);
+        if (error.response) {
+          toast.error(error.response?.data);
+        }
+      }
+    }
+
+    console.log(requestBody);
+  };
+
   const handleSearch = async (query) => {
     console.log(process.env.REACT_APP_EDAMAM_API_ENDPOINT);
+    setCompletionLoading(true);
     try {
       const response = await axios.get(
         process.env.REACT_APP_EDAMAM_API_AUTOCOMPLETE_ENDPOINT,
@@ -66,7 +158,9 @@ function DietBox() {
 
       const completions = response.data;
       setSuggestions(completions);
+      setCompletionLoading(false);
     } catch (error) {
+      setCompletionLoading(false);
       console.log(error);
     }
   };
@@ -108,6 +202,17 @@ function DietBox() {
 
   const handleChange = (selected) => {
     setMeals(selected);
+  };
+
+  const handleChangeLog = (value, index) => {
+    console.log(value, index);
+
+    setFoodItems((prevMeals) => {
+      const updatedMeals = [...prevMeals];
+      updatedMeals[index].food = value;
+      return updatedMeals;
+    });
+    console.log(foodItems);
   };
 
   const handleSaveDietPlan = async () => {
@@ -193,6 +298,7 @@ function DietBox() {
                   id="basic-typeahead-multiple"
                   labelKey="name"
                   multiple
+                  isLoading={completionLoading}
                   onInputChange={handleSearch}
                   onChange={handleChange}
                   options={suggestions}
@@ -252,11 +358,77 @@ function DietBox() {
           <Container className="pop-up-container">
             <h3>Log Food</h3>
             <Form>
-              <Form.Group>
-                <Form.Control type="text" placeholder="Search for food..." />
-              </Form.Group>
-              <Button variant="outline-primary" onClick={() => {}}>
-                Search
+              {foodItems.map((foodItem, index) => (
+                <Row key={index}>
+                  <Col>
+                    <Typeahead
+                      id={`basic-typeahead-single-${index}`}
+                      labelKey="name"
+                      onInputChange={handleSearch}
+                      onChange={(value) => handleChangeLog(value, index)}
+                      options={suggestions}
+                      placeholder="Search for your favorite meal.."
+                      selected={foodItem.food}
+                    />
+                  </Col>
+
+                  <Col>
+                    <InputGroup className="pt-2">
+                      <InputGroup.Text>Qty (in g) :</InputGroup.Text>
+                      <Form.Control
+                        type="number"
+                        placeholder="Quantity"
+                        value={foodItem.quantity}
+                        onChange={(e) => handleQuantityChange(e, index)}
+                      />
+                    </InputGroup>
+                  </Col>
+                </Row>
+              ))}{" "}
+              <Row>
+                <Col>
+                  <Button
+                    className="mt-3"
+                    variant="outline-primary"
+                    onClick={handleAddRow}
+                  >
+                    Add
+                  </Button>
+                </Col>
+                <Col>
+                  <Button
+                    className="mt-3"
+                    variant="outline-primary"
+                    onClick={handleRemoveRow}
+                  >
+                    Remove
+                  </Button>
+                </Col>
+              </Row>
+              <div className="mt-3">
+                <ToggleButtonGroup
+                  type="radio"
+                  defaultValue={mealTiming}
+                  name="options"
+                  onChange={(timing) => setMealTiming(timing)}
+                >
+                  <ToggleButton id="tbg-radio-1" value="breakfast">
+                    Breakfast
+                  </ToggleButton>
+                  <ToggleButton id="tbg-radio-2" value="lunch">
+                    Lunch
+                  </ToggleButton>
+                  <ToggleButton id="tbg-radio-3" value="dinner">
+                    Dinner
+                  </ToggleButton>
+                </ToggleButtonGroup>
+              </div>
+              <Button
+                className="mt-2"
+                variant="outline-primary"
+                onClick={handleFoodLog}
+              >
+                Log diet
               </Button>
             </Form>
           </Container>
