046d6782bd248c59a42e97930eb9fe7c0f09344f
Devadathan M B <devadathanmb@gmail.com>
2023-05-27 22:24:44 +0530

    Bold food item name and quantity, make food item name title cased

--- src/components/DietPage/FoodItemCard.jsx
+++ src/components/DietPage/FoodItemCard.jsx
@@ -7,8 +7,12 @@ function FoodItemCard({ diet }) {
       <Card>
         <Card.Img variant="top" src={diet.image} />
         <Card.Body>
-          <Card.Title>{diet.item}</Card.Title>
-          <Card.Text>Quantity : {diet.quantity.toFixed(3)}</Card.Text>
+          <Card.Title className="pb-2 fw-bold">
+            {diet.item.replace(/\b\w/g, (c) => c.toUpperCase())}
+          </Card.Title>
+          <Card.Text className="fw-bold">
+            Quantity : {diet.quantity.toFixed(3)} g
+          </Card.Text>
           <Card.Text>Carbs per gram : {diet.carbs.toFixed(3)}</Card.Text>
           <Card.Text>Fat per gram : {diet.fat.toFixed(3)}</Card.Text>
           <Card.Text>Protien per gram : {diet.protien.toFixed(3)}</Card.Text>
