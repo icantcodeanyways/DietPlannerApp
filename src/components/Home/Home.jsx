23bbdefbb58e9c7638e7796cc239ec4b105b63c5
Devadathan M B <devadathanmb@gmail.com>
2023-05-27 20:10:38 +0530

    Remove unwanted styling, add message instead of placeholder

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,25 +5,25 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div>
-      <div className="Conatiner1">
-        <div
-          className="Box col-xs-6"
-          style={{ float: "right", position: "absolute", right: "20px" }}
-        >
-          <div className="TextSection">
-            <h1 style={{ color: "white" }}>Sample Text </h1>
-            <h3 style={{ color: "white" }}>Sample Text </h3>
-          </div>
-          <Link to="/register">
-            <Button variant="primary" style={{ margin: 10 }}>
-              SignUp
-            </Button>
-          </Link>
-          <Link to="/login">
-            <Button variant="warning">Login</Button>
-          </Link>
+    <div className="Conatiner1">
+      <div
+        className="Box col-xs-6"
+        style={{ float: "right", position: "absolute", right: "25px" }}
+      >
+        <div className="TextSection">
+          <h1 style={{ color: "white" }}>Welcome to Diet Planner App</h1>
+          <h5 className="pt-3" style={{ color: "white" }}>
+            Login to start using the app!
+          </h5>
         </div>
+        <Link to="/register">
+          <Button variant="primary" style={{ margin: 10 }}>
+            SignUp
+          </Button>
+        </Link>
+        <Link to="/login">
+          <Button variant="warning">Login</Button>
+        </Link>
       </div>
     </div>
   );
