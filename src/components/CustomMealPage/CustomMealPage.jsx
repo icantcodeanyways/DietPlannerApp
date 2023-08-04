a45fe20690f139edf9adc367a5935693ba0dc5c4
Devadathan M B <devadathanmb@gmail.com>
2023-08-04 21:10:59 +0530

    feat: custom meal logging feature added

--- src/components/CustomMealPage/CustomMealPage.jsx
+++ src/components/CustomMealPage/CustomMealPage.jsx
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Button, InputGroup, Container, Form, Row, Col } from "react-bootstrap";
+import {
+  Button,
+  InputGroup,
+  Container,
+  Form,
+  Row,
+  Col,
+  Modal,
+} from "react-bootstrap";
 import "react-bootstrap-typeahead/css/Typeahead.css";
 import { Typeahead } from "react-bootstrap-typeahead";
 import axios from "axios";
 import "./styles.css";
 import Loading from "../Loading/Loading";
 import { toast } from "react-toastify";
+import jwt_decode from "jwt-decode";
 
 function CustomMealPage() {
   const [foodItems, setFoodItems] = useState([{ food: "", quantity: "" }]);
@@ -18,7 +27,12 @@ function CustomMealPage() {
   });
 
   const [showNutritionalInfo, setShowNutritionalInfo] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState({ status: false, message: "" });
+
+  // Save to database related state
+  const [showSaveDialog, setShowSaveDialog] = useState(false);
+  const [mealName, setMealName] = useState("");
+  const [mealTiming, setMealTiming] = useState("");
 
   useEffect(() => {
     const preFetch = async () => {
@@ -85,7 +99,10 @@ function CustomMealPage() {
       /* console.log(`${itemName} ${itemQuantity}`); */
 
       try {
-        setIsLoading(true);
+        setIsLoading({
+          status: true,
+          message: "Calculating nutritional info..",
+        });
         const response = await axios.get(
           process.env.REACT_APP_EDAMAM_FOOD_DATABASE_ENDPOINT,
           {
@@ -139,7 +156,7 @@ function CustomMealPage() {
         totalCalories += info.calorie * info.quantity;
       });
 
-      setIsLoading(false);
+      setIsLoading({ status: false, message: "" });
       setShowNutritionalInfo(true);
       setNutritionalInfo({
         totalCalories: totalCalories.toFixed(2),
@@ -154,6 +171,7 @@ function CustomMealPage() {
       console.log(error);
     }
   };
+
   const handleQuantityChange = (e, index) => {
     const value = e.target.value;
     setFoodItems((prevFoodItems) => {
@@ -167,6 +185,7 @@ function CustomMealPage() {
     if (foodItems.length === 1) {
       setFoodItems([{ food: "", quantity: "" }]);
       setSuggestions([]);
+      setShowNutritionalInfo(false);
       return;
     }
     setFoodItems((prevFoodItems) => {
@@ -186,72 +205,184 @@ function CustomMealPage() {
     });
   };
 
+  const handleSaveDialogOpen = () => {
+    setShowSaveDialog(true);
+  };
+
+  const handleSaveDialogClose = () => {
+    setShowSaveDialog(false);
+  };
+
+  const handleSaveMeal = async () => {
+    // Validate meal name and timing
+    if (!mealName.trim() || !mealTiming.trim()) {
+      toast.error("Please enter both meal name and timing.");
+      return;
+    }
+
+    const totalQuantity = foodItems.reduce((total, item) => {
+      return total + parseFloat(item.quantity);
+    }, 0);
+
+    const totalFoodData = {
+      meal_timing: mealTiming.toLowerCase(),
+      meal_name: mealName,
+      total_calories: parseFloat(nutritionalInfo.totalCalories),
+      total_protien: parseFloat(nutritionalInfo.totalProtien),
+      quantity: totalQuantity,
+      total_carbs: parseFloat(nutritionalInfo.totalCarbs),
+      total_fat: parseFloat(nutritionalInfo.totalFat),
+    };
+
+    try {
+      const token = localStorage.getItem("Token");
+      const userId = jwt_decode(token).user_id;
+      setIsLoading({ status: true, message: "Logging custom meal.." });
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_ENDPOINT}/api/users/${userId}/custom_meal_log`,
+        totalFoodData,
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+
+      setIsLoading({ status: false, message: "" });
+      toast.success("Custom meal saved successfully!");
+
+      setMealName("");
+      setMealTiming("");
+
+      setShowSaveDialog(false);
+    } catch (error) {
+      setIsLoading(false);
+      toast.error("An error occurred while saving the custom meal.");
+      console.log(error);
+    }
+  };
+
   return (
     <div className="d-flex custom-meal-page align-items-center vh-100">
-      {isLoading && <Loading message="Calculating nutritional info.." />}
+      {isLoading.status && <Loading message={isLoading.message} />}
       <Container className="pop-up-container">
         <h3>Custom meal calculation</h3>
-        <Form>
-          {foodItems.map((foodItem, index) => (
-            <Row key={index}>
-              <Col>
-                <Typeahead
-                  id={`basic-typeahead-single-${index}`}
-                  options={suggestions}
-                  labelKey="name"
-                  onInputChange={handleSearch}
-                  onChange={(value) => handleChangeIngredients(value, index)}
-                  placeholder="Search the ingredient.."
-                  selected={foodItem.food}
-                />
-              </Col>
-
-              <Col>
-                <InputGroup className="pt-2">
-                  <InputGroup.Text>Qty (in g) :</InputGroup.Text>
-                  <Form.Control
-                    type="number"
-                    placeholder="Quantity"
-                    value={foodItem.quantity}
-                    onChange={(e) => handleQuantityChange(e, index)}
-                  />
-                </InputGroup>
-              </Col>
-            </Row>
-          ))}{" "}
-          <Row>
+        {foodItems.map((foodItem, index) => (
+          <Row key={index}>
             <Col>
-              <Button className="mt-3" variant="success" onClick={handleAddRow}>
-                Add
-              </Button>
+              <Typeahead
+                id={`basic-typeahead-single-${index}`}
+                options={suggestions}
+                labelKey="name"
+                onInputChange={handleSearch}
+                onChange={(value) => handleChangeIngredients(value, index)}
+                placeholder="Search the ingredient.."
+                selected={foodItem.food}
+              />
             </Col>
+
             <Col>
-              <Button
-                className="mt-3"
-                variant="danger"
-                onClick={handleRemoveRow}
-              >
-                Remove
-              </Button>
+              <InputGroup className="pt-2">
+                <InputGroup.Text>Qty (in g) :</InputGroup.Text>
+                <Form.Control
+                  type="number"
+                  placeholder="Quantity"
+                  value={foodItem.quantity}
+                  onChange={(e) => handleQuantityChange(e, index)}
+                />
+              </InputGroup>
             </Col>
           </Row>
-          <Button
-            className="mt-3"
-            variant="outline-primary"
-            onClick={handleCalculateDiet}
-          >
-            Calculate meal info
-          </Button>
-          {showNutritionalInfo && (
-            <div className="mt-3 fw-bold fs-6">
-              <p className="fs-3">Nutritonal info</p>
-              <p>Total calories : {nutritionalInfo.totalCalories} cal</p>
-              <p>Total fat : {nutritionalInfo.totalFat} g</p>
-              <p>Total protien : {nutritionalInfo.totalProtien} g</p>
-              <p>Total carbs : {nutritionalInfo.totalCarbs} g</p>
-            </div>
-          )}
-        </Form>
+        ))}
+        <Row>
+          <Col>
+            <Button className="mt-3" variant="success" onClick={handleAddRow}>
+              Add
+            </Button>
+          </Col>
+          <Col>
+            <Button className="mt-3" variant="danger" onClick={handleRemoveRow}>
+              Remove
+            </Button>
+          </Col>
+        </Row>
+        <Button
+          className="mt-3"
+          variant="outline-primary"
+          onClick={handleCalculateDiet}
+        >
+          Calculate meal info
+        </Button>
+        {showNutritionalInfo && (
+          <div className="mt-3 fw-bold fs-6">
+            <p className="fs-3">Nutritonal info</p>
+            <p>Total calories : {nutritionalInfo.totalCalories} cal</p>
+            <p>Total fat : {nutritionalInfo.totalFat} g</p>
+            <p>Total protien : {nutritionalInfo.totalProtien} g</p>
+            <p>Total carbs : {nutritionalInfo.totalCarbs} g</p>
+            <Button
+              className="mt-3"
+              variant="primary"
+              onClick={handleSaveDialogOpen}
+            >
+              Save to Database
+            </Button>
+          </div>
+        )}
+        <Modal show={showSaveDialog} onHide={handleSaveDialogClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Save Custom Meal</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Group>
+              <Form.Label>Meal Name:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter meal name"
+                value={mealName}
+                onChange={(e) => setMealName(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="pt-3">
+              <div>
+                <Button
+                  variant={
+                    mealTiming === "Breakfast" ? "primary" : "outline-primary"
+                  }
+                  onClick={() => setMealTiming("Breakfast")}
+                  className="me-2"
+                >
+                  Breakfast
+                </Button>
+                <Button
+                  variant={
+                    mealTiming === "Lunch" ? "primary" : "outline-primary"
+                  }
+                  onClick={() => setMealTiming("Lunch")}
+                  className="me-2"
+                >
+                  Lunch
+                </Button>
+                <Button
+                  variant={
+                    mealTiming === "Dinner" ? "primary" : "outline-primary"
+                  }
+                  onClick={() => setMealTiming("Dinner")}
+                >
+                  Dinner
+                </Button>
+              </div>
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleSaveDialogClose}>
+              Cancel
+            </Button>
+            <Button variant="primary" onClick={handleSaveMeal}>
+              Save
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Container>
     </div>
   );
